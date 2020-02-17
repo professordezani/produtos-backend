68893dc4528c010de626a1a7645978f91eeca6bb
JunioGrigorio <j.gregore@gmail.com>
2020-02-17 23:26:22 +0000

    Versão 1

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const server = express();
+
+var produtos = [
+    {id: 1, nome: 'Computador', preco: 1200.20 },
+    {id: 2, nome: 'Mouse', preco: 20.50 },
+];
+
+server.get('/produto', function(request, response) {
+    return response.json(produtos);
+})
+
+server.get('/produto/:id', function(request, response) {
+
+    const id = request.params.id;
+
+    const produto = produtos.filter(p => p.id == id);
+
+    return response.json(produto);
+})
+
+server.listen(3000);
