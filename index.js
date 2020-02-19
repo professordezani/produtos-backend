711cce5d29b344c3b6b257eb8a938bd0de1d0fc2
Prof. Dr. Henrique Dezani <dezani@fatecriopreto.edu.br>
2020-02-19 01:27:09 +0000

    Versão da aula 18/02/20

--- index.js
+++ index.js
@@ -8,17 +8,46 @@ var produtos = [
     { id: 4, nome: 'Teclado', preco: 220.50 },
 ];
 
+// middlewares:
+server.use(express.json());
+
 server.get('/produto', function(request, response) {
     return response.json(produtos);
 })
 
-server.get('/produto/:id', function(request, response) {
-    
+server.get('/produto/:id', function(request, response) {    
     const id = request.params.id;
-
     const produto = produtos.filter(p => p.id == id);
-
     return response.json(produto);
 })
+
+server.post('/produto', function(request, response) {
+    const produto = request.body;
+    produtos.push(produto);
+    return response.status(201).send();
+})
+
+server.delete('/produto/:id', function(request, response) {
+    const id = request.params.id;
+    produtos = produtos.filter(p => p.id != id);
+    return response.status(200).send();
+})
+
+server.put('/produto/:id', (req, res) => {
+    const id = req.params.id;
+    const produto = req.body;
+
+    produtos.forEach(p => {
+        if(p.id == id) {
+            p.nome = produto.nome;
+            p.preco = produto.preco;
+            return;
+        }
+    })
+
+    return res.send();
+})
+
+
 
 server.listen(3000);
