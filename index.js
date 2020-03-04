9a1038f73eb3723868f380f0d09616e5e3431a0b
Prof. Dr. Henrique Dezani <dezani@fatecriopreto.edu.br>
2020-03-04 01:14:22 +0000

    versão da aula 03/02/2020

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const MONGO_URL ="mongodb+srv://user:senha@clusterlp3noite-cokoy.mongodb.net/dbp
 
 const db = mongoose.connect(MONGO_URL, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false
 });
 
 var produtos = [];
@@ -17,42 +18,48 @@ var produtos = [];
 server.use(express.json());
 
 server.get('/produto', async function(request, response) {
-    const produtos = await Produto.find({nome: "Computador"});
+    const produtos = await Produto.find();
     return response.json(produtos);
 })
 
-server.get('/produto/:id', function(request, response) {    
+server.get('/produto/:id', async function(request, response) {    
     const id = request.params.id;
-    const produto = produtos.filter(p => p.id == id);
+    // const produto = produtos.filter(p => p.id == id);
+    const produto = await Produto.findById(id);
     return response.json(produto);
 })
 
-server.post('/produto', function(request, response) {
+server.post('/produto', async function(request, response) {
     const produto = request.body;
-    Produto.create(produto);
+    await Produto.create(produto);  //.catch(error => {});
     // produtos.push(produto);
     return response.status(201).send();
 })
 
-server.delete('/produto/:id', function(request, response) {
+server.delete('/produto/:id', async function(request, response) {
     const id = request.params.id;
-    produtos = produtos.filter(p => p.id != id);
+    // produtos = produtos.filter(p => p.id != id);
+    await Produto.findByIdAndRemove(id);
     return response.status(200).send();
 })
 
-server.put('/produto/:id', (req, res) => {
+server.put('/produto/:id', async (req, res) => {
     const id = req.params.id;
     const produto = req.body;
 
-    produtos.forEach(p => {
-        if(p.id == id) {
-            p.nome = produto.nome;
-            p.preco = produto.preco;
-            return;
-        }
-    })
+    await Produto.findByIdAndUpdate(id, produto);
 
     return res.send();
+
+    // produtos.forEach(p => {
+    //     if(p.id == id) {
+    //         p.nome = produto.nome;
+    //         p.preco = produto.preco;
+    //         return;
+    //     }
+    // })
+
+    
 })
 
 
